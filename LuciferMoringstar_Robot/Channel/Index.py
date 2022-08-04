85413af9275231c6a2c6c5959a81b7be1dfa5288
MuhammedRahees <88150389+MuhammedRahees@users.noreply.github.com>
2022-08-04 18:37:26 +0530

    Update Index.py

--- LuciferMoringstar_Robot/Channel/Index.py
+++ LuciferMoringstar_Robot/Channel/Index.py
@@ -80,8 +80,3 @@ async def index_files(bot, message):
                 await msg.edit(f'Total {total_files} Saved To DataBase!')
 
 RATING = ["5.1 | IMDB", "6.2 | IMDB", "7.3 | IMDB", "8.4 | IMDB", "9.5 | IMDB", ]
-GENRES = ["fun, fact",
-         "Thriller, Comedy",
-         "Drama, Comedy",
-         "Family, Drama",
-         "Action, Adventure"]
